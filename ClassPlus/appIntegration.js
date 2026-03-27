a421d2ff4abc3412defa61333ede8a6bf2e1fcb8
Syed Faizan Ali <syedfaizanali1450@gmail.com>
2026-03-27 11:03:27 +0530

    Fixed error logs

--- ClassPlus/appIntegration.js
+++ ClassPlus/appIntegration.js
@@ -4,6 +4,18 @@ import { sendEmail } from "../mails/sendEmail.js";
 
 const appIntegration = asyncHandler(async (webhookBody) => {
     console.log(`🎬 ClassPlus App Integration`);
+
+    const payload = webhookBody?.payload;
+
+    // ✅ Validation
+    if (!payload?.object) {
+        console.error("Invalid webhook body: missing payload.object")
+        return 
+    }
+
+    const date = convertDateFormat(payload.object.start_time);
+    const batchName = payload.object.topic?.trim() || "Zoom Recording";
+
     const classplusIntegrationEndpoint = process.env.CLASSPLUS_INTEGRATION
     try {
         const response = await axios.post(
@@ -17,17 +29,17 @@ const appIntegration = asyncHandler(async (webhookBody) => {
         );
 
         let message = response.data?.message;
-        let batchName = response.data?.payload?.batchName;
-        let date = response.data?.payload?.date;
+        // let batchName = response.data?.payload?.batchName;
+        // let date = response.data?.payload?.date;
         let url = response.data?.payload?.url;
 
         await sendEmail({ batchName, date, url, message }, "classplus_success");
 
     } catch (error) {
         if (error.response?.data?.isIgnore) return;
-        let message = error.response?.data?.message;
+        let message = error.response?.data?.message || error.message;;
         let batchName = error.response?.data?.payload?.batchName || "Unknown";
-        let date = error.response?.data?.payload?.date || new Date().toLocaleString("en-IN", {timeZone: "Asia/Kolkata"});
+        let date = error.response?.data?.payload?.date || new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" });
         let url = error.response?.data?.payload?.url;
 
         await sendEmail({ batchName, date, url, message }, "classplus_failure");
@@ -35,5 +47,25 @@ const appIntegration = asyncHandler(async (webhookBody) => {
     }
 
 });
+
+function convertDateFormat(dateStr) {
+    const options = {
+        timeZone: "Asia/Kolkata",
+        hour12: false,
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    };
+
+    const parts = new Intl.DateTimeFormat("en-GB", options).formatToParts(new Date(dateStr));
+
+    const map = {};
+    parts.forEach(p => map[p.type] = p.value);
+
+    return `${map.day}-${map.month}-${map.year} ${map.hour}:${map.minute}:${map.second}`;
+}
 
 export default appIntegration;
